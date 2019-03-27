97d9352c8cb147be48799a96278614318ffe138a
Chris Mattinson <chris@x1.localdomain>
2019-03-26 21:35:54 -0600

    Remove images

--- app/routes/players.js
+++ app/routes/players.js
@@ -7,37 +7,31 @@ export default Route.extend({
       name: 'Nikita Kucherov',
       team: 'Tampa Bay Lightning',
       points: "119",
-      image: 'https://upload.wikimedia.org/wikipedia/commons/c/cb/Crane_estate_(5).jpg',
     },{
       id: 'flames-scorer',
       name: 'Johnny Gaudreau',
       team: 'Calgary Flames',
       points: "92",
-      image: 'https://upload.wikimedia.org/wikipedia/commons/c/cb/Crane_estate_(5).jpg',
     }, {
       id: 'sharks-scorer',
       name: 'Brent Burns',
       team: 'San Jose Sharks',
       points: "75",
-      image: 'https://upload.wikimedia.org/wikipedia/commons/c/cb/Crane_estate_(5).jpg',
     }, {
       id: 'jets-scorer',
       name: 'Blake Wheeler',
       team: 'Winnipeg Jets',
       points: "88",
-      image: 'https://upload.wikimedia.org/wikipedia/commons/c/cb/Crane_estate_(5).jpg',
     }, {
       id: 'bruins-scorer',
       name: 'Brad Marchand',
       team: 'Boston Bruins',
       points: "87",
-      image: 'https://upload.wikimedia.org/wikipedia/commons/c/cb/Crane_estate_(5).jpg',
     }, {
       id: 'oilers-scorer',
       name: 'Connor McDavid',
       team: 'Edmonton Oilers',
       points: "105",
-      image: 'https://upload.wikimedia.org/wikipedia/commons/c/cb/Crane_estate_(5).jpg',
     }];
   }
 });
